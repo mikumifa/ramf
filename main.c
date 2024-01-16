8dbebcd2d2caac766794251c03a3d687a9cdcf82
root <root@DESKTOP-VBE51HD>
2024-01-16 17:24:37 +0800

    compile

--- main.c
+++ main.c
@@ -3,10 +3,21 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "stdio.h"
 
 int main() {
     init_ramfs();
     init_shell();
-    printf("%s", getPrePathFromFullPath("//"));
+
+    assert(sls("/home") == 1);
+    assert(scat("/home/ubuntu/.bashrc") == 1);
+    assert(scat("/") == 1);
+    assert(smkdir("/home") == 0);
+    assert(smkdir("/test/1") == 1);
+    assert(stouch("/home/1") == 0);
+    assert(smkdir("/home/1/1") == 1);
+    assert(stouch("/test/1") == 1);
+    assert(swhich("notexist") == 1);
+
+    close_shell();
+    close_ramfs();
 }
