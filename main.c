4d2e9317c5a8f50594311e292ea444a1d3f84c78
root <root@DESKTOP-VBE51HD>
2024-01-16 16:37:48 +0800

    compile

--- main.c
+++ main.c
@@ -1,23 +1,14 @@
-#include "ramfs.h"
-#include "shell.h"
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-
-int main() {
-    init_ramfs();
-    init_shell();
-
-    assert(sls("/home") == 1);
-    assert(scat("/home/ubuntu/.bashrc") == 1);
-    assert(scat("/") == 1);
-    assert(smkdir("/home") == 0);
-    assert(smkdir("/test/1") == 1);
-    assert(stouch("/home/1") == 0);
-    assert(smkdir("/home/1/1") == 1);
-    assert(stouch("/test/1") == 1);
-    assert(swhich("notexist") == 1);
-
-    close_shell();
-    close_ramfs();
-}
+cat /home/ubuntu/.bashrc
+        export PATH=/usr/bin/
+export PATH=/home:$PATH
+        touch /home/ls
+        touch /home///ls
+which ls
+/home/ls
+        touch /usr/bin/ls
+        which ls
+/home/ls
+        echo hello world\n
+        hello worldn
+        echo \$PATH is $PATH
+        $PATH is /home:/usr/bin/
