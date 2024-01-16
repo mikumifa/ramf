f9c202dd8578af853bede5aa6f9608a176a4e907
root <root@DESKTOP-VBE51HD>
2024-01-16 18:33:37 +0800

    compile

--- main.c
+++ main.c
@@ -1,49 +1,32 @@
 #include "ramfs.h"
 #include "shell.h"
-#include <string.h>
-#include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
-const char *content = "export PATH=/usr/bin/\n";
+extern node *root;
+const char *content = "export PATH=$PATH:/usr/bin/\n";
 const char *ct = "export PATH=/home:$PATH";
 int main() {
     init_ramfs();
 
-    assert(rmkdir("/home") == 0);
-    assert(rmkdir("//home") == -1);
-    assert(rmkdir("/test/1") == -1);
-    assert(rmkdir("/home/ubuntu") == 0);
-    assert(rmkdir("/usr") == 0);
-    assert(rmkdir("/usr/bin") == 0);
-    assert(rwrite(ropen("/home///ubuntu//.bashrc", O_CREAT | O_WRONLY), content, strlen(content)) == strlen(content));
-
-    int fd = ropen("/home/ubuntu/.bashrc", O_RDONLY);
-    char buf[105] = {0};
-
-    assert(rread(fd, buf, 100) == strlen(content));
-    assert(!strcmp(buf, content));
-    assert(rwrite(ropen("/home////ubuntu//.bashrc", O_WRONLY | O_APPEND), ct, strlen(ct)) == strlen(ct));
-    memset(buf, 0, sizeof(buf));
-    assert(rread(fd, buf, 100) == strlen(ct));
-    assert(!strcmp(buf, ct));
-    assert(rseek(fd, 0, SEEK_SET) == 0);
-    memset(buf, 0, sizeof(buf));
-    assert(rread(fd, buf, 100) == strlen(content) + strlen(ct));
-    char ans[205] = {0};
-    strcat(ans, content);
-    strcat(ans, ct);
-    assert(!strcmp(buf, ans));
+    smkdir("/home");
+    smkdir("/home/ubuntu");
+    smkdir("/usr");
+    smkdir("/usr/bin");
+    stouch("/home/ubuntu/.bashrc");
+    rwrite(ropen("/home/ubuntu/.bashrc", O_WRONLY), content, strlen(content));
+    rwrite(ropen("/home/ubuntu/.bashrc", O_WRONLY | O_APPEND), ct, strlen(ct));
+    scat("/home/ubuntu/.bashrc");
 
     init_shell();
-
-    assert(scat("/home/ubuntu/.bashrc") == 0);
-    assert(stouch("/home/ls") == 0);
-    assert(stouch("/home///ls") == 0);
-    assert(swhich("ls") == 0);
-    assert(stouch("/usr/bin/ls") == 0);
-    assert(swhich("ls") == 0);
-    assert(secho("hello world\\n") == 0);
-    assert(secho("\\$PATH is $PATH") == 0);
-    close_shell();
+    swhich("ls");
+    stouch("/usr/bin/ls");
+    swhich("ls");
+    stouch("/home/ls");
+    swhich("ls");
+    secho("hello world");
+    secho("The Environment Variable PATH is:\\$PATH");
     close_ramfs();
+    close_shell();
+    assert(root==NULL);
 }
