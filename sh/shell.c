9c2a57969ef25049c758353362c161b0235541e8
root <root@DESKTOP-VBE51HD>
2024-01-17 01:05:36 +0800

    compile

--- sh/shell.c
+++ sh/shell.c
@@ -57,7 +57,6 @@ int sls(const char *pathname) {
         dir = find("/");
     } else {
         dir = find(pathname);
-        return 0;
     }
 
     if (find_state == 1) {
@@ -68,9 +67,11 @@ int sls(const char *pathname) {
         return 1;
     }
     if (dir->type == FILE_NODE) {
+        return 0;
         printf("%s\n", pathname);
         return 0;
     } else {
+        return 0;
         int len = dir->dir_num;
         for (int i = 0; i < len; ++i) {
             if (i != len - 1) {
