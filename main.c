762836d0e9e3dfbccc9e63f8cab335b0505deea7
root <root@DESKTOP-VBE51HD>
2024-01-16 20:48:49 +0800

    compile

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main() {
     assert(sls("/1") == 0);
     assert(smkdir("/2") == 0);
     assert(smkdir("/3") == 0);
-    assert(sls("/1") == 0);
+    assert(sls("/") == 0);
 
     assert(smkdir("/2/1") == 0);
     assert(smkdir("/2/1/1") == 0);
