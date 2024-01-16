97ef05025d804afcf0a2b3b6ea910cecd0dd8641
root <root@DESKTOP-VBE51HD>
2024-01-16 21:17:14 +0800

    compile

--- sh/shell.c
+++ sh/shell.c
@@ -67,10 +67,10 @@ int sls(const char *pathname) {
         printf("ls: cannot access '%s': No such file or directory\n", pathname);
         return 1;
     }
-//    if (dir == NULL) {
-//        printf("ls: cannot access '%s': No such file or directory\n", pathname);
-//        return 1;
-//    }
+    if (dir == NULL) {
+//            printf("ls: cannot access '%s': No such file or directory\n", pathname);
+        return 1;
+    }
 
     if (dir->type == FILE_NODE) {
 //        printf("%s\n", pathname);
