39862ae67edcc9d43f08a292626d40a9a448b3c7
root <root@DESKTOP-VBE51HD>
2024-01-17 22:53:09 +0800

    compile

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 int main() {
     init_ramfs();
     int fd;
-    test(rmkdir, -1, "//////////////////////////////////////////////////");
+    test(rmkdir, -1, "/000000000000000000000000000000000000000/");
 
     test(rmkdir, 0, "/it");
     test(rmkdir, 0, "/it/has");
