6ba43895e92f00f1f0544beb737c072f4e18c9b0
root <root@DESKTOP-VBE51HD>
2024-01-17 15:04:56 +0800

    compile

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main() {
         char str[50]; // 存储转换结果的字符数组，长度要足够大
         // 使用sprintf函数进行转换
         // %d表示转换为十进制整数，str是存储结果的字符数组
-        sprintf(str, "/%d/%d/", i);
+        sprintf(str, "/%d/%d/", i,i);
         assert(rmkdir(str) == 0);
     }
     close_ramfs();
