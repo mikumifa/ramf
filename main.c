94eecc172ab2cf648628774a95a56e1f3d0cc45c
root <root@DESKTOP-VBE51HD>
2024-01-17 23:02:34 +0800

    compile

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main() {
     test(rmkdir, -1, "/000000000000000000000000000000000000000/");
 
     test(rmkdir, 0, "/it");
-    test(rmkdir, 0, "it");
+    test(rmkdir, 0, "/it ");
 
     test(rmkdir, 0, "/it/has");
     test(rmkdir, 0, "/it/has/been");
