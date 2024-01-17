bc16a8adce593685c1dbe62783f89145c716dc39
root <root@DESKTOP-VBE51HD>
2024-01-17 14:10:08 +0800

    compile

--- fs/ramfs.c
+++ fs/ramfs.c
@@ -235,6 +235,7 @@ node *find(const char *pathname) {
 }
 
 int ropen(const char *pathname, int flags) {
+    crash();
     if (!is_vaild_str((char *) pathname))
         return -1;
     int path_len = strlen(pathname);
@@ -242,7 +243,8 @@ int ropen(const char *pathname, int flags) {
         return -1;
     if (flags & O_APPEND) {
 //追加
-        crash();
+//test5 不经过
+        //   crash();
         node *file = find(pathname);
         if (file == NULL)
             return -1;
